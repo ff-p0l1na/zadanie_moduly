93e5bbfe80e364407a5988332486c1db4130b21d
Paulina F <faltynp@gmail.com>
2023-02-17 20:46:37 +0100

    odczyt, modyfikacja i zapis do nowego pliku csv

--- reader.py
+++ reader.py
@@ -12,13 +12,13 @@ zmiana = sys.argv[ARG_ZMIANY:]  # format zmiany to "x,y,wartosc", moze ich byc n
 wartosci_do_zmiany = []
 for n in range(ARG_ZMIANY, len(sys.argv)):  # arg zmiany to co do zasady bedzie 3, a koniec to n, stad dwukropek
     komorka_i_tresc = sys.argv[n].split(',')  # 'x,y,tresc' czyli 'kolumna,wiersz,tresc'
-    wybrana_kolumna = int(komorka_i_tresc[0])  # to tylko zamysl usera, jeszcze nie idx z csv
-    wybrany_wiersz = int(komorka_i_tresc[1])  # same here, to jeszcze nie idx z csv
-    tresc = komorka_i_tresc[2] # to sie nie zmieni
-# przerobienie wybranych wartosci na indexy listy
-    zmieniany_index_kolumna = int(wybrana_kolumna - 1)  # same but different, z userowego na kompowy
-    zmieniany_index_wiersz = int(wybrany_wiersz - 1)  # same but different, z userowego na kompowy
-    wartosci_do_zmiany.append([zmieniany_index_kolumna, zmieniany_index_wiersz, tresc]) # wdz = [[1,2,tresc], [2,3,trresc]]
+    wybrana_kolumna = int(komorka_i_tresc[0])  # to tylko zamysl usera, jeszcze nie idx z csv, x
+    wybrany_wiersz = int(komorka_i_tresc[1])  # same here, to jeszcze nie idx z csv, y
+    tresc = komorka_i_tresc[2]  # "cokolwiek"
+# przerobienie wybranych wartosci na indexy listy:
+    zmieniany_index_kolumna = int(wybrana_kolumna - 1)  # same but different, z userowego na kompowy, x
+    zmieniany_index_wiersz = int(wybrany_wiersz - 1)  # same but different, z userowego na kompowy, y
+    wartosci_do_zmiany.append([zmieniany_index_kolumna, zmieniany_index_wiersz, tresc])  # wdz = [[1,2,tresc], [x,y,cokolwiek]]
 #
 # zmiana zawartosci pliku wejsciowego na liste list:
 with open(plik_wejscia, "r") as pwe:
@@ -28,12 +28,11 @@ with open(plik_wejscia, "r") as pwe:
 for element in wartosci_do_zmiany:
 # dla elementow mojej listy list, np:
 # wartosci_do_zmiany =
-# [[1,1,zmiana],
-# [2,2,zmiana2]]
+# [[x,y,cokolwiek], [1,2,zmiana]]
 # "zmieniany_index_kolumna" bedzie miec wartosc pierwszego el. listy "wartosci do zmiany" and so on
     zmieniany_index_kolumna, zmieniany_index_wiersz, tresc = element
-    zawartosc_pliku[zmieniany_index_wiersz][zmieniany_index_kolumna] = tresc
-# zawartosc_pliku przyklad:
+    zawartosc_pliku[zmieniany_index_wiersz][zmieniany_index_kolumna] = tresc  # idx wiersza na liscie 'zaw.pliku' wybiera konkretna liste, idx kolumny wybiera nr elementu wybranej listy
+# zawartosc_pliku[][] np:
 # [[a,b,c], <- a to zp[1][1]
 # [d,e,f,], <- f to zp[2][3]
 # [g,h,i]]
